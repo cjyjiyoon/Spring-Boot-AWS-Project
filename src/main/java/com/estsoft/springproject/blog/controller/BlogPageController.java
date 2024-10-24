ea354533b11430848b04857e516fd8fe3baa9b34
cjy <cjiyoon1130@gmail.com>
2024-10-24 15:39:29 +0900

    10/24 오류 수정

--- src/main/java/com/estsoft/springproject/blog/controller/BlogPageController.java
+++ src/main/java/com/estsoft/springproject/blog/controller/BlogPageController.java
@@ -3,7 +3,6 @@ package com.estsoft.springproject.blog.controller;
 import com.estsoft.springproject.blog.domain.Article;
 import com.estsoft.springproject.blog.domain.dto.ArticleViewResponse;
 import com.estsoft.springproject.blog.service.BlogService;
-import com.estsoft.springproject.user.domain.Users;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -40,14 +38,10 @@ public class BlogPageController {  // 뷰컨트롤러
     // GET /articles/{id} 상세페이지 리턴
     @GetMapping("/articles/{id}")
     public String showDetails(@PathVariable Long id, Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Users users = (Users) authentication.getPrincipal();
-
         Article article = blogService.findBy(id);
-
         model.addAttribute("article", new ArticleViewResponse(article));
 
-        return "article";
+        return "article";   // article.html
     }
 
     // GET /new-articles?id=1
